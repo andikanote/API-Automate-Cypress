6a32508ecf582e4e721a568d36e56884c65f3c1c
Andika Prananto <68476780+andikanote@users.noreply.github.com>
2022-05-30 12:34:43 +0700

    commit api

--- cypress/integration/API/GET_Users.js
+++ cypress/integration/API/GET_Users.js
@@ -6,38 +6,39 @@ describe('api test cases',() => {
     it('get user', () => {        
         cy.request({
             method : 'GET',
-            url : 'https://gorest.co.in/public-api/users/4038/posts',
+            // url : 'https://gorest.co.in/public-api/users/4038/posts',
+            url : 'https://gorest.co.in/public-api/users',
             headers: {
                 'Authorization': 'Bearer '+accesstoken,
               }
         }).then((res)=>{
             expect(res.status).to.eq(200)
-            expect(res.body.meta.pagination).has.property('limit',20)
-            expect(res.body).has.to.deep.equal({
-                "code": 200,
-                "meta": {
-                    "pagination": {
-                        "total": 0,
-                        "pages": 0,
-                        "page": 1,
-                        "limit": 20
-                    }
-                },
-                "data": []
-                })
+            // expect(res.body.meta.pagination).has.property('limit',20)
+            // expect(res.body).has.to.deep.equal({
+            //     "code": 200,
+            //     "meta": {
+            //         "pagination": {
+            //             "total": 0,
+            //             "pages": 0,
+            //             "page": 1,
+            //             "limit": 20
+            //         }
+            //     },
+            //     "data": []
+            //     })
         })
     })
 
-    // it('get user by id', () => {
-    //     cy.request({
-    //         method : 'GET',
-    //         url : 'https://gorest.co.in/public-api/users/2',
-    //         headers: {
-    //             'authorization': 'Bearer '+accesstoken,
-    //           }
-    //     }).then((res)=>{
-    //         expect(res.status).to.eq(200)
-    //         expect(res.body.data.name).to.eq('Shwet Acharya')
-    //     })    
-    // })  
+    it('get user by id', () => {
+        cy.request({
+            method : 'GET',
+            url : 'https://gorest.co.in/public-api/users/2',
+            headers: {
+                'authorization': 'Bearer '+accesstoken,
+              }
+        }).then((res)=>{
+            expect(res.status).to.eq(200)
+            expect(res.body.data.name).to.eq('Shwet Acharya')
+        })    
+    })  
 })
